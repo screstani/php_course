3e10087a48449ff6e109fb43b8b54d1dc75c2188
Sayonara <sayonara.crestani@gmail.com>
2023-01-30 19:00:31 -0300

    arranging arrays

--- php_arrays/notas.php
+++ php_arrays/notas.php
@@ -4,9 +4,17 @@ $notas = [
     'Vinicius' => 6,
     'João' => 8,
     'Ana' => 10,
-    'Roberto' => 7,
+    'Roberto' => null,
     'Maria' => 9,
 ];
 
 krsort($notas);
 var_dump($notas);
+
+var_dump(array_key_exists('Ana', $notas));
+var_dump(isset($notas['Roberto']));
+var_dump(in_array(10, $notas, true));
+
+//array_key_exists
+//in_array (o terceiro parametro setado como true, faz a verificacao estrita ===)
+//isset
