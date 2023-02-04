70e6a79d308d0343e605a8fcc64a3fa2b448795d
Sayonara <sayonara.crestani@gmail.com>
2023-02-04 06:36:00 -0300

    oop programming in php

---
+++ php_oop/src/Modelo/Conta/Titular.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Banco\Modelo\Conta;
+
+use Alura\Banco\Modelo\Pessoa;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+
+class Titular extends Pessoa
+{
+    private $endereco;
+
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
+    {
+        parent::__construct($nome, $cpf);
+        $this->endereco = $endereco;
+    }
+
+    public function recuperaEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
+}
---
+++ php_oop/teste-saque.php
@@ -0,0 +1,21 @@
+<?php
+
+use Alura\Banco\Modelo\Conta\ContaPoupanca;
+use Alura\Banco\Modelo\Conta\ContaCorrente;
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\CPF;
+use Alura\Banco\Modelo\Endereco;
+
+require_once 'autoload.php';
+
+$conta = new ContaPoupanca(
+    new Titular(
+        new CPF('123.456.789-10'),
+        'Vinicius Dias',
+        new Endereco('Petrópolis', 'bairro Teste', 'Rua lá', '37')
+    )
+);
+$conta->deposita(500);
+$conta->saca(100);
+
+echo $conta->recuperaSaldo();
